7c256e9d39179fa579d2861860a3bb5edb448714
Julien Klaer <klaer.j@sfeir.com>
2020-05-27 15:58:03 +0200

    Possibility to create multiple probe from one file.
    See #2

--- internal/service/client.go
+++ internal/service/client.go
@@ -115,33 +115,35 @@ func DeleteWithParam(endpoint endpoints.Endpoint, urlParameters map[string]strin
 
 // Post is a convenient way to apply a POST request to a specified endpoint.
 func Post(endpoint endpoints.Endpoint, yamlProbe parser.HttpProbe) error {
-	// TODO: manage the creation of all probes set in the yaml. Maybe new endpoint ?
-	// TODO: pass CreateProbeRequest as a parameter so we can pass whatever payload we want.
-	cpr := CreateProbeRequest{
-		Name:  yamlProbe.Http[0].Name,
-		URL:   yamlProbe.Http[0].URL,
-		Delay: yamlProbe.Http[0].Delay,
-	}
-	jsonCpr, err := marshalToJSON(cpr)
-	if err != nil {
-		return err
-	}
+	// TODO: Handles now multiple probes in a single file but we do
+	// TODO: 1 request per probe creation. Madprobe should take an array for probe creation.
+	for _, probe := range yamlProbe.Http {
+		cpr := CreateProbeRequest{
+			Name:  probe.Name,
+			URL:   probe.URL,
+			Delay: probe.Delay,
+		}
+		jsonCpr, err := marshalToJSON(cpr)
+		if err != nil {
+			return err
+		}
 
-	url := endpoint.GetURL()
-	resp, err := http.Post(url, jsonMimeType, strings.NewReader(jsonCpr))
-	if err != nil {
-		return fmt.Errorf("error occured when doing the request. %v\n", err)
-	}
-	defer resp.Body.Close()
+		url := endpoint.GetURL()
+		resp, err := http.Post(url, jsonMimeType, strings.NewReader(jsonCpr))
+		if err != nil {
+			return fmt.Errorf("error occured when doing the request. %v\n", err)
+		}
+		defer resp.Body.Close()
 
-	bodyData, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return fmt.Errorf("could not read response body. %v\n", err)
-	}
-	if resp.StatusCode < 200 || resp.StatusCode > 299 {
-		return fmt.Errorf("server returned an error: %v. status code %d\n", strings.TrimSpace(string(bodyData)), resp.StatusCode)
-	}
+		bodyData, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return fmt.Errorf("could not read response body. %v\n", err)
+		}
+		if resp.StatusCode < 200 || resp.StatusCode > 299 {
+			return fmt.Errorf("server returned an error: %v. status code %d\n", strings.TrimSpace(string(bodyData)), resp.StatusCode)
+		}
 
-	fmt.Println(string(bodyData))
+		fmt.Println(string(bodyData))
+	}
 	return nil
 }
